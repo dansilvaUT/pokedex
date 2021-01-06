7c8f184c1497cb5687a5b66a50b12703450d19fd
dansilvaUT <dan13silv@gmail.com>
2021-01-05 19:24:22 -0800

    Added Pokemon component

--- src/components/MyPokemon.js
+++ src/components/MyPokemon.js
@@ -1,14 +1,16 @@
-import {Component} from 'react';
+import { Component } from 'react';
+
+import Pokemon from './Pokemon';
 
 class MyPokemon extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <section>
-                {this.props.pokemon.map((poke, index)=>(
-                    <section key={index}>
-                        <div>{poke.name}</div>
-                        <img src={poke.image} alt={poke.name}/>
-                    </section>
+                {this.props.pokemon.map((poke, index) => (
+                    <Pokemon
+                        key={index}
+                        pokemon={poke}
+                    />
                 ))}
             </section>
         )
---
+++ src/components/Pokemon.js
@@ -0,0 +1,16 @@
+const Pokemon = ({ pokemon }) => {
+    return (
+        <section>
+            <h3>{pokemon.name}</h3>
+            <img src={pokemon.image} alt={pokemon.name} />
+            <h5>Abilites:</h5>
+            <ul>
+                {pokemon.abilities.map((power, index) => (
+                    <li key={index}>{power.ability.name}</li>
+                ))}
+            </ul>
+        </section>
+    )
+}
+
+export default Pokemon;
